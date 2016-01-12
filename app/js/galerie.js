17cba139106cfbd305973ce5516708bca48ac592
Nath <tholfou@gmail.com>
2016-01-12 19:52:26 +0100

    adaptation de la galerie en responsive

--- app/js/galerie.js
+++ app/js/galerie.js
@@ -10,9 +10,19 @@ $(function(){
 
 function previous(){
 	$('.precedent').off('click');
+	var tailleThumb = $('.thumbnails').innerWidth();
+	var tailleDeroulant = $('#deroulant').innerWidth();
+	var delta = tailleDeroulant;
+	
+	var posThumb = parseInt($('.thumbnails').css('left'));
+	var newPosThumb = posThumb + delta;
 
-	if(parseInt($('.thumbnails').css('left'))<0 ){
-		$('.thumbnails').animate({left:'+=350'},500);
+	if(posThumb<0 && newPosThumb<=0){
+		$('.thumbnails').animate({left:'+=' + delta},500);
+
+	}else if(posThumb<0 && newPosThumb>0){
+		delta = delta - newPosThumb;
+		$('.thumbnails').animate({left:'+=' + delta},500);
 	}
 
 	setTimeout(function(){
@@ -22,9 +32,20 @@ function previous(){
 
 function next(){
 	$('.suivant').off('click');
+	var tailleThumb = $('.thumbnails').innerWidth();
+	var tailleDeroulant = $('#deroulant').innerWidth();
+	var delta =tailleDeroulant;
 
-	if(parseInt($('.thumbnails').css('left'))>-350 ){
-		$('.thumbnails').animate({left:'-=350'},500);
+	var posThumb = parseInt($('.thumbnails').css('left'));
+	var newPosThumb = posThumb - delta;
+	var posMax = -(tailleThumb - tailleDeroulant);
+
+	if(posThumb>posMax && newPosThumb>=posMax){
+		$('.thumbnails').animate({left:'-=' + delta},500);
+
+	}else if(posThumb>posMax && newPosThumb<posMax){
+		delta = delta + newPosThumb - posMax;
+		$('.thumbnails').animate({left:'-=' + delta},500);
 	}
 
 	setTimeout(function(){
