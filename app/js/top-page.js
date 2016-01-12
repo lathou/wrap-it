d445d835ce2b2d31546944a1b7b287f08f48e26d
Nath <tholfou@gmail.com>
2016-01-12 17:55:54 +0100

    JS : gestion du placement du bouton top-page par rapport au footer

--- app/js/top-page.js
+++ app/js/top-page.js
@@ -1,9 +1,24 @@
 $(function(){
-	$(window).on('scroll', function(){
+	$(window).on('scroll resize load', function(){
 		if ($(window).scrollTop()>300){
 			$('.top-page').show();
 		}else if($(window).scrollTop()<=300){
 			$('.top-page').hide();
+		}
+
+
+		//gestion du placement du bouton top-page par rapport au footer
+		var scrollBottom = 0-($(window).scrollTop() + $(window).height()-$('body').height());
+
+		if(scrollBottom < $('footer').innerHeight()){
+			$('.top-page').css('bottom', $('footer').innerHeight()-scrollBottom+10);
+
+		}else{
+			if($(window).width()>991){
+				$('.top-page').css('bottom', '30px');
+			}else{
+				$('.top-page').css('bottom', '5px');
+			}
 		}
 	});
 
